6a1bda07ef90ad21a7330cf16a26d75ef48903b7
DESKTOP-V7FTJ2B\Louis Gaillet <louis.jeu2@laposte.net>
2019-03-24 18:47:36 +0100

    add populate request

--- denzelapp.js
+++ denzelapp.js
@@ -1,3 +1,5 @@
+const Express = require("express");
+var app = Express();
 const URL_MONGO = 'mongodb+srv://louis:1234@webapp-admoy.mongodb.net/test?retryWrites=true'; // URL of my cluster
 const MongoClient = require('mongodb').MongoClient;
 const CLUSTER_NAME = 'Webapp'; // cluster name
@@ -7,12 +9,34 @@ const COLLECTION_NAME = 'denzelmovies';
 const imdb = require('./src/imdb');
 const DENZEL_IMDB_ID = 'nm0000243';
 
-//listen part of API
-MongoClient.connect(URL_MONGO,{ useNewUrlParser: true }, function(err, client) {
-    if(err) {
-         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-    }
-    console.log('Connected to ' + DB_NAME + ' database, to collection ' + COLLECTION_NAME);
-    const collection = client.db(DB_NAME).collection(COLLECTION_NAME);
-    client.close();
- });
+var collection;
+
+//connection to mongodb
+app.listen(9292, () => {
+    MongoClient.connect(URL_MONGO,{ useNewUrlParser: true }, function(err, client) {
+        if(err) {
+            console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+        }
+        console.log('Connected to ' + DB_NAME + ' database, to collection ' + COLLECTION_NAME);
+        collection = client.db(DB_NAME).collection(COLLECTION_NAME);
+    });
+});
+
+app.get("/movies/populate", (request, response) => {
+    if(!collection){
+        return response.status(500).send('The database is not connected');
+        console.log('error, pas de collection')
+      }
+      imdb(DENZEL_IMDB_ID).then((val)=>{
+        movies = val;
+        collection.insertMany(movies,(error,result)=>{
+          if(error){
+            return res.status(500).send(error);
+            console.log('error: could not load movies')
+          }
+          console.log('populating successful of '+ result.result.n + "  movies");
+          retour = {"total" : result.result.n};
+          response.send(retour);
+        });
+      });    
+});
