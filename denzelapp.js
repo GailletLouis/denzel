8c16cafd92c880b3e386c3ed543a205ccf3a8d07
DESKTOP-V7FTJ2B\Louis Gaillet <louis.jeu2@laposte.net>
2019-03-24 02:04:02 +0100

    adding mongo connect

--- denzelapp.js
+++ denzelapp.js
@@ -1,2 +1,18 @@
 const URL_MONGO = 'mongodb+srv://louis:1234@webapp-admoy.mongodb.net/test?retryWrites=true'; // URL of my cluster
+const MongoClient = require('mongodb').MongoClient;
 const CLUSTER_NAME = 'Webapp'; // cluster name
+const DB_NAME = 'moviedb';
+const COLLECTION_NAME = 'denzelmovies';
+
+const imdb = require('./src/imdb');
+const DENZEL_IMDB_ID = 'nm0000243';
+
+//listen part of API
+MongoClient.connect(URL_MONGO,{ useNewUrlParser: true }, function(err, client) {
+    if(err) {
+         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+    }
+    console.log('Connected to ' + DB_NAME + ' database, to collection ' + COLLECTION_NAME);
+    const collection = client.db(DB_NAME).collection(COLLECTION_NAME);
+    client.close();
+ });
