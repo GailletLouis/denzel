6c7cc1de3375669d83ee06cc3f03e2a38069898a
DESKTOP-V7FTJ2B\Louis Gaillet <louis.jeu2@laposte.net>
2019-03-24 01:31:42 +0100

    adding cluster name

--- denzelapp.js
+++ denzelapp.js
@@ -1,1 +1,2 @@
-const url_mongo = 'mongodb+srv://louis:1234@webapp-admoy.mongodb.net/test?retryWrites=true'; // URL of my cluster
+const URL_MONGO = 'mongodb+srv://louis:1234@webapp-admoy.mongodb.net/test?retryWrites=true'; // URL of my cluster
+const CLUSTER_NAME = 'Webapp'; // cluster name
